7bc7308a241f81dbfc1641ed30fcebbca740d2ef
EleazarTDP <eleazarcabrera@techdatapeople.com>
2022-03-17 13:22:24 -0600

    conectar a db

--- src/index.js
+++ src/index.js
@@ -1,9 +1,17 @@
-const expres = require('express');
+const express = require('express');
+const { MongoClient } = require('mongodb');
 
-const app = expres();
+require('dotenv').config('./');
 
+
+const app = express();
+const db = require('./utils/database');
 
 const port = process.env.PORT || 7799;
+
+app.unsubscribe(express.static('public'));
+app.use(express.json({ extended: false}));
+
 
 app.use('/prueba', require('./api/prueba/routes'))
 app.use('/trabajadores', require('./api/trabajadores/routes'))
@@ -11,7 +19,10 @@ app.use('/nominas', require('./api/nominas/routes'))
 app.use('/users', require('./api/users/routes'))
 
 
-
-app.listen(port, () => {
-    console.log('Server started on port', port);
+db.connection().then(mongo => {
+    app.listen(port, () => {
+        console.log('Server started on port', port);
+    })
+}).catch(err => {
+    console.error(err);
 })
