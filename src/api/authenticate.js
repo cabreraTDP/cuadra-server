8ea37d43277f63754a28993b0d16169c6fe221b7
EleazarTDP <eleazarcabrera@techdatapeople.com>
2022-03-29 14:07:17 -0600

    decode token in authorizer

--- src/api/authenticate.js
+++ src/api/authenticate.js
@@ -6,7 +6,9 @@ module.exports = (req, res, next) => {
 
     if(token){
         const info = decodeJWT(token);
-        req.userId = info.id;
+        req.user = info.id;
+        req.cliente = info.id;
+
         next();
 
     }else{
