17f91b3f02bf134de60687ee8e48322c7665630b
EleazarTDP <eleazarcabrera@techdatapeople.com>
2022-03-29 14:27:58 -0600

    create nomina endpoint

--- src/api/nominas/nominas.js
+++ src/api/nominas/nominas.js
@@ -1,6 +1,5 @@
-////////////////////////////////////////////////////////////////////////////////////////////////////
-////                                         prueba                                             ////
-////////////////////////////////////////////////////////////////////////////////////////////////////
+const Nomina = require('../../models/Nomina');
+const Trabajador = require('../../models/Trabajador');
 
 const prueba = (req, res) => {
     console.log('Prueba');
@@ -8,9 +7,55 @@ const prueba = (req, res) => {
     res.status(200).json({
         modulo: "nominas"
     })
-}
+};
+
+const createNomina = (req, res) => {
+
+    const {
+        periodoInicio, periodoFin, semana,
+        operaciones
+    } = req.body;
+
+    const { user:idUsuario, cliente:idCliente } = req;
+
+
+    //operaciones es un array
+    const registros = operaciones.map((operacion) => (
+        {
+            trabajador: operacion.trabajador,
+            sueldoBase: operacion.sueldoBase,
+            dias: operacion.dias,
+            complementos: operacion.complementos,
+            rebajes: operacion.rebajes,
+            isr: (operacion.sueldoBase*operacion.dias)*0.1,
+            totalPagar: (operacion.sueldoBase*operacion.dias)+operacion.complementos-operacion.rebajes-(operacion.sueldoBase*operacion.dias)*0.1 //ISR
+        }
+    ));
+
+    const newNomina = new Nomina({
+        identificacion: {
+            cliente: idCliente,
+            usuario: idUsuario
+        },
+        detalle: {
+            periodoInicio,
+            periodoFin,
+            semana
+        },
+        registros
+    });
+
+    await newNomina.save()
+
+    res.status(200).json({
+        msg: "Success"
+    })
+};
+
+
 
 
 module.exports = {
-    prueba
+    prueba,
+    createNomina
 }
--- src/api/nominas/routes.js
+++ src/api/nominas/routes.js
@@ -1,11 +1,15 @@
 const express = require('express');
 const router = express.Router();
-const { prueba } = require('./nominas')
+const authenticate = require('../authenticate');
+const { prueba, createNomina } = require('./nominas')
 
 // @route   POST api/prueba
 // @desc    Probar la api
 // @access  Private
 
 router.get('/', prueba);
+
+router.post('/add', authenticate, createNomina);
+
 
 module.exports = router;
