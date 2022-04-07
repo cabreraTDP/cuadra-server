f1dd94677e062d0e3b3a793965e80822ad2c921a
EleazarTDP <eleazarcabrera@techdatapeople.com>
2022-04-06 23:01:29 -0500

    create uploadFile endpoint for Trabajadores

--- src/api/trabajadores/routes.js
+++ src/api/trabajadores/routes.js
@@ -1,14 +1,18 @@
 const express = require('express');
 const router = express.Router();
 const authenticate = require('../authenticate');
-const { prueba, createTrabajador, editTrabajador, getTrabajadores } = require('./trabajadores')
+const { prueba, createTrabajador, editTrabajador, getTrabajadores, uploadFile } = require('./trabajadores')
+const { uploading } = require('../../utils/s3');
 
 // @route   POST api/prueba
 // @desc    Probar la api
 // @access  Private
+router.post('/ejemplo',prueba);
+
+router.post('/uploadFile', authenticate, uploading.single('file'),uploadFile);
 
 router.get('/', authenticate, getTrabajadores);
-router.post('/add', authenticate, createTrabajador);
+router.post('/add',  authenticate, createTrabajador);
 router.post('/edit', authenticate, editTrabajador);
 
 
