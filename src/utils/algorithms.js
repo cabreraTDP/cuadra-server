3f42547e4206851101a2fe2b1b2adf4971b650ec
GuillermoTDP <guillermomartinez@techdatapeople.com>
2022-05-17 19:04:58 -0500

    Impuestos Agregados

--- src/utils/algorithms.js
+++ src/utils/algorithms.js
@@ -5,7 +5,7 @@ const Ingresos = (texto) => {
     const receptor = []
     const emisor = []
 
-    let cuentatotal=0
+    let cuentatotal = 0
     let cuentafecha = 0
     let cuentaemisor = 0
     let cuentareceptor = 0
@@ -18,46 +18,46 @@ const Ingresos = (texto) => {
     const StringEfecto = "Efecto del Comprobante:"
 
 
-    for(let i=0; i<texto.length; i++){
+    for (let i = 0; i < texto.length; i++) {
         let line = texto[i];
 
-        if(cuentaefecto == 1){
+        if (cuentaefecto == 1) {
             listahora.push(line);
             cuentaefecto = 0
         };
 
-        if(cuentatotal == 1){
+        if (cuentatotal == 1) {
             total.push(line)
             cuentatotal = 0
         };
 
-        if(cuentafecha == 1){ 
+        if (cuentafecha == 1) {
             fecha.push(line)
             cuentafecha = 0
         };
 
-        if(cuentareceptor == 1){
+        if (cuentareceptor == 1) {
             receptor.push(line)
             cuentareceptor = 0
         };
 
-        if(cuentaemisor == 1){
+        if (cuentaemisor == 1) {
             emisor.push(line)
             cuentaemisor = 0
         };
 
-        if(String3 === line) cuentatotal = 1
-        if(StringFecha === line) cuentafecha = 1
-        if(StringEmisor === line) cuentaemisor = 1
-        if(StringReceptor === line) cuentareceptor = 1
-        if(StringEfecto === line) cuentaefecto = 1
+        if (String3 === line) cuentatotal = 1
+        if (StringFecha === line) cuentafecha = 1
+        if (StringEmisor === line) cuentaemisor = 1
+        if (StringReceptor === line) cuentareceptor = 1
+        if (StringEfecto === line) cuentaefecto = 1
 
     };
 
 
     return (
         listahora.map((efecto, i) => {
-            if(efecto === "Ingreso"){
+            if (efecto === "Ingreso") {
                 return {
                     tipo: "Ingreso",
                     fecha: fecha[i],
@@ -65,12 +65,97 @@ const Ingresos = (texto) => {
                     receptor: receptor[i],
                     total: total[i]
                 }
-            }else{
+            } else {
                 return {}
             }
         }).filter((objeto) => Object.keys(objeto).length > 0)
     )
 
 }
+const Impuestos = (texto) => {
+    listaRFC = []
+    listaMes = []
+    listaYear = []
+    listaArchivo = []
+    listaIVA = []
+    listaISR = []
+    listaTotal = []
+    listaTipos = []
 
-module.exports = {Ingresos}
+    contadorRFC = 0
+    contadorMes = 0
+    contadorYear = 0
+    contadorArchivo = 0
+    contadorIVA = 0
+    contadorISR = 0
+    contadorTotal = 0
+
+    StringRFC = "RFC:"
+    StringMes = "Período de la declaración:"
+    StringYear = "Ejercicio:"
+    //En el archivo original de Py
+    //El contenido de StringTotal es: "^a pagar:"
+    StringTotal = "Importe total a pagar:"
+    StringISR = "Impuesto a cargo:"
+    StringComplementaria = "Complementaria"
+
+    Archivo = "Impuestos"
+    for (let i = 0; i < texto.length; i++) {
+        let line = texto[i];
+
+        if (StringComplementaria != null) {
+            Archivo = "Salarios"
+        };
+        if (contadorRFC == 1) {
+            listaRFC.push(line)
+            contadorRFC = 0
+        };
+        if (contadorMes == 1) {
+            listaMes.push(line)
+            contadorMes = 0
+        };
+        if (contadorYear == 1) {
+            listaYear.push(line)
+            contadorYear = 0
+        };
+        //No estoy muy seguro si el If contadorArchivo debe de ir
+        if (contadorArchivo == 1) {
+            listaArchivo.push(line)
+            contadorArchivo = 0
+        };
+        if (contadorISR == 1) {
+            listaISR.push(line)
+            contadorISR = 0
+        };
+        if (contadorTotales == 1) {
+            listaTotal.push(line)
+            contadorTotales = 0
+        };
+
+        if (StringRFC === line) contadorRFC = 1
+        if (StringMes === line) contadorMes = 1
+        if (StringComplementaria === line) contadorArchivo = 1
+        if (StringISR === line) contadorISR = 1
+        if (StringTotal === line) contadorTotal = 1
+
+        listaTipos.push(Archivo)
+    };
+
+    for (let i = 0; i < listaArchivo.length; i++) {
+        tipoArchivo = listaTipos[i]
+        if (tipoArchivo == "Impuestos") {
+            ISR = parseInt(listaISR[i])
+            Total = parseInt(listaTotal[i])
+            listaIVA.push(Total - ISR)
+        }
+        else {
+            listaIVA.push(0)
+            listaISR[i] = listaTotal[i]
+        }
+    }
+}
+//ISR-IVA
+//
+
+
+module.exports = { Ingresos, Impuestos }
